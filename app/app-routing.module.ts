dbad3648e4a8d3630eaaa598d1a2a6b801f5f41b
npietri <nicolaspietri.fr@gmail.com>
2019-10-29 14:11:50 +0100

    Updates

---
+++ app/app-routing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from "@angular/core";
+//import { CommonModule } from "@angular/common";
+import { RouterModule, Routes } from "@angular/router";
+import { ActualiteComponent } from "./actualite/actualite.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { AssociationComponent } from "./association/association.component";
+import { AdherentComponent } from "./adherent/adherent.component";
+import { PrestationComponent } from "./prestation/prestation.component";
+
+const routes: Routes = [
+  { path: "", redirectTo: "/association", pathMatch: "full" },
+  { path: "association", component: AssociationComponent },
+  {
+    path: "actualite",
+    component: ActualiteComponent
+  },
+  {
+    path: "prestation",
+    component: PrestationComponent
+  },
+  {
+    path: "adherent",
+    component: AdherentComponent
+  },
+  { path: "**", component: PageNotFoundComponent }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ app/app.module.ts
@@ -0,0 +1,49 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+import { AppComponent } from "./app.component";
+import { HttpClientModule } from "@angular/common/http";
+import { AgmCoreModule } from "@agm/core";
+import { RouterModule, Routes } from "@angular/router";
+
+import { MDBBootstrapModule } from "angular-bootstrap-md";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { FormsModule } from "@angular/forms";
+import { ActualiteComponent } from "./actualite/actualite.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { AssociationComponent } from "./association/association.component";
+import { AdherentComponent } from "./adherent/adherent.component";
+import { PrestationComponent } from "./prestation/prestation.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { FooterComponent } from "./footer/footer.component";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ActualiteComponent,
+    AssociationComponent,
+    AdherentComponent,
+    PrestationComponent,
+    PageNotFoundComponent,
+    NavbarComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MDBBootstrapModule.forRoot(),
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    AgmCoreModule.forRoot({
+      apiKey: "AIzaSyC8eXMauUESsEeaJCaIT0fG9drTjPpPilk"
+    }),
+    RouterModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
